c8a30f5b60f97e9bbefe9279670e0dadee442872
PowellMedlock <PAM17130@ucmo.edu>
2025-04-21 17:51:23 -0500

    url analysis
    added the tools to analyze URLs

---
+++ back-end/backend/src/main/java/com/backend/backend/UrlAnalysisResult.java
@@ -0,0 +1,21 @@
+package com.example.my_backend.Domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UrlAnalysisResult {
+
+    private String responseCode;
+    private String cipherSuite;
+    private Map<String, String> headers;
+    private List<CertificateInfoDTO> certificates;
+
+
+}
